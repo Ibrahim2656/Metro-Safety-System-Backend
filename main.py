a7837de938ff3a0b743b843d9a3b81beee758bcf
Ibrahim2656 <ibrahimahmed2656@gmail.com>
2024-10-19 16:44:21 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from pydantic import BaseModel
-from Algorithm import main
+from Algorthim import main
 
 app = FastAPI()
 
@@ -15,13 +14,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Define a Pydantic model for the request body
-class VideoRequest(BaseModel):
-    drive_link: str
-
 @app.post("/process-video/")
-async def process_video_endpoint(request: VideoRequest):
-    main(request.drive_link)
+async def process_video_endpoint(drive_link: str):
+    main(drive_link)
     
     return FileResponse('Output_video/output_with_full_annotations.mp4', media_type="video/mp4", filename="output_video.mp4")
 
